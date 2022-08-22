c24f3d5df8147652e73d5332d6dccc54dba3b9cc
VasylAndrusiak <45362386+VasylAndrusiak@users.noreply.github.com>
2022-08-22 08:22:46 +0300

    Add test class
    Added test class which answers the question

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+Message message = await botClient.SendTextMessageAsync(
+    chatId: chatId,
+    text: "Trying *all the parameters* of `sendMessage` method",
+    parseMode: ParseMode.MarkdownV2,
+    disableNotification: true,
+    replyToMessageId: update.Message.MessageId,
+    replyMarkup: new InlineKeyboardMarkup(
+        InlineKeyboardButton.WithUrl(
+            "Check sendMessage method",
+            "https://core.telegram.org/bots/api#sendmessage")),
+    cancellationToken: cancellationToken);
